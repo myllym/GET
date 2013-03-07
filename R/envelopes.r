c8a56947d05ee457d3803dc1069877981fd456d2
Henri Seijo <henri.seijo@iki.fi>
2013-03-07 15:56:20 +0200

    Tabs to spaces.

--- R/envelopes.r
+++ R/envelopes.r
@@ -13,56 +13,56 @@
 #' res <- rank_envelope(curve_set)
 #' plot(res)
 rank_envelope <- function(curve_set, alpha=0.05, ...) {
-	# Lm    = the vector of L-function values for data
-	# stats = matrix where each row contains L function values of a simulation under null hypothesis
-	# Nsim  = number of simulations, that is nrow(stats) --> This argument not needed
-	# alpha = the chosen significance level of the test
+    # Lm    = the vector of L-function values for data
+    # stats = matrix where each row contains L function values of a simulation under null hypothesis
+    # Nsim  = number of simulations, that is nrow(stats) --> This argument not needed
+    # alpha = the chosen significance level of the test
 
-	curve_set <- spptest:::convert_envelope(curve_set)
+    curve_set <- spptest:::convert_envelope(curve_set)
 
-	data_curve <- curve_set[['obs']]
-	stats <- t(curve_set[['sim_m']])
+    data_curve <- curve_set[['obs']]
+    stats <- t(curve_set[['sim_m']])
 
-	Nsim <- dim(stats)[1];
-	n <- dim(stats)[2]
-	MX <- apply(stats, MARGIN=2, FUN=median)
-	statsLm <- rbind(stats,Lm);
-	RR <- apply(statsLm, MARGIN=2, FUN=rank)
-	Rmin <- apply(RR, MARGIN=1, FUN=min)
-	Rmax <- apply(RR, MARGIN=1, FUN=max)
-	Rmax <- Nsim+2-Rmax
-	RmRm <- rbind(Rmin,Rmax)
-	distance <- apply(RmRm, MARGIN=2, FUN=min)
+    Nsim <- dim(stats)[1];
+    n <- dim(stats)[2]
+    MX <- apply(stats, MARGIN=2, FUN=median)
+    statsLm <- rbind(stats,Lm);
+    RR <- apply(statsLm, MARGIN=2, FUN=rank)
+    Rmin <- apply(RR, MARGIN=1, FUN=min)
+    Rmax <- apply(RR, MARGIN=1, FUN=max)
+    Rmax <- Nsim+2-Rmax
+    RmRm <- rbind(Rmin,Rmax)
+    distance <- apply(RmRm, MARGIN=2, FUN=min)
 
-	#-- calculate the p-value
+    #-- calculate the p-value
     u <- -distance
     p <- spptest:::estimate_p_value(obs=u[Nsim+1], sim_vec=u[1:Nsim], ...)
-#	pm <- 1;pl <- 1;pu <- 1;
-#	for(i in 1:Nsim) {
-#		if (distance[i]<distance[Nsim+1]) {pm<-pm+1;pl<-pl+1;pu<-pu+1;}
-#		if (distance[i]==distance[Nsim+1]) {pm<-pm+1/2;pu<-pu+1;}
-#	}
-#	pm <- pm/(Nsim+1);
-#	pl <- pl/(Nsim+1);
-#	pu <- pu/(Nsim+1);
+    #    pm <- 1;pl <- 1;pu <- 1;
+    #    for(i in 1:Nsim) {
+    #        if (distance[i]<distance[Nsim+1]) {pm<-pm+1;pl<-pl+1;pu<-pu+1;}
+    #        if (distance[i]==distance[Nsim+1]) {pm<-pm+1/2;pu<-pu+1;}
+    #    }
+    #    pm <- pm/(Nsim+1);
+    #    pl <- pl/(Nsim+1);
+    #    pu <- pu/(Nsim+1);
 
-	#-- calculate the 100(1-alpha)% envelopes
+    #-- calculate the 100(1-alpha)% envelopes
     distancesorted <- sort(distance[-(Nsim+1)]);
     kalpha <- distancesorted[round((alpha)*Nsim)];
-	LB <- array(0, n);
-	UB <- array(0, n);
+    LB <- array(0, n);
+    UB <- array(0, n);
 
-	for(i in 1:n){
-		Hod <- sort(stats[,i])
-		LB[i]<- Hod[kalpha];
-		UB[i]<- Hod[Nsim-kalpha+1];
-	}
+    for(i in 1:n){
+        Hod <- sort(stats[,i])
+        LB[i]<- Hod[kalpha];
+        UB[i]<- Hod[Nsim-kalpha+1];
+    }
 
-	res <- list(r=curve_set[['r']], method="Rank envelope test", p=p,
+    res <- list(r=curve_set[['r']], method="Rank envelope test", p=p,
                 central_curve=MX, data_curve=data_curve, lower=LB, upper=UB,
-			    call=match.call())
-	class(res) <- "envelope_test"
-	res
+                call=match.call())
+    class(res) <- "envelope_test"
+    res
 }
 
 print.envelope_test <- function(x, ...) {
@@ -70,13 +70,13 @@ print.envelope_test <- function(x, ...) {
 }
 
 plot.envelope_test <- function(x, ...) {
-	with(x, {
-		plot(r, data_curve, ylim=c(min(data_curve,lower,upper,central_curve), max(data_curve,lower,upper,central_curve)), type="l", lty=1, lwd=2,
-             main=method, ...) #, xlab=expression(r), ylab=expression(S(r))
-		lines(r, lower, lty=2)
-		lines(r, upper, lty=2)
-		lines(r, central_curve, lty=1)
-	})
+    with(x, {
+         plot(r, data_curve, ylim=c(min(data_curve,lower,upper,central_curve), max(data_curve,lower,upper,central_curve)), type="l", lty=1, lwd=2,
+              main=method, ...) #, xlab=expression(r), ylab=expression(S(r))
+         lines(r, lower, lty=2)
+         lines(r, upper, lty=2)
+         lines(r, central_curve, lty=1)
+                })
 }
 
 #' Variance stabilized envelope test
@@ -94,30 +94,30 @@ plot.envelope_test <- function(x, ...) {
 #' plot(res, xlab=expression(italic(r)), ylab=expression(italic(L(r)-r)))
 sd_envelope <- function(curve_set, alpha=0.05, ...) {
 
-	curve_set <- spptest:::convert_envelope(curve_set)
+    curve_set <- spptest:::convert_envelope(curve_set)
 
-	data_curve <- curve_set[['obs']]
-	stats <- t(curve_set[['sim_m']])
+    data_curve <- curve_set[['obs']]
+    stats <- t(curve_set[['sim_m']])
 
-	Nsim <- dim(stats)[1];
-	n <- dim(stats)[2]
-	EX <- colMeans(stats);
+    Nsim <- dim(stats)[1];
+    n <- dim(stats)[2]
+    EX <- colMeans(stats);
     sdX <- as.vector(apply(stats, MARGIN=2, FUN=sd))
 
-#	for(i in 1:n) {
-#		if(sdX[i]==0) {
-#			sdX[i]<-0.00000001
-#		}
-#	}
+    #    for(i in 1:n) {
+    #        if(sdX[i]==0) {
+    #            sdX[i]<-0.00000001
+    #        }
+    #    }
 
-	distance <- array(0, Nsim);
-	for(j in 1:Nsim) {
+    distance <- array(0, Nsim);
+    for(j in 1:Nsim) {
         ttt <- abs(stats[j,]-EX)/sdX
         ttt[!is.finite(ttt)] <- 0
-		distance[j] <- max(ttt);
-	}
+        distance[j] <- max(ttt);
+    }
 
-	distancesorted <- sort(distance);
+    distancesorted <- sort(distance);
 
     ttt <- abs(data_curve-EX)/sdX;
     ttt[!is.finite(ttt)] <- 0
@@ -127,13 +127,13 @@ sd_envelope <- function(curve_set, alpha=0.05, ...) {
     p <- spptest:::estimate_p_value(obs=tmaxd, sim_vec=distance, ...)
 
     #-- calculate the 100(1-alpha)% envelopes
-	talpha <- distancesorted[round((1-alpha)*Nsim)];
+    talpha <- distancesorted[round((1-alpha)*Nsim)];
     LB <- EX - talpha*sdX;
     UB <- EX + talpha*sdX;
 
     res <- list(r=curve_set[['r']], method="Variance stabilized envelope test", p=p,
-            central_curve=EX, data_curve=data_curve, lower=LB, upper=UB,
-            call=match.call())
+                central_curve=EX, data_curve=data_curve, lower=LB, upper=UB,
+                call=match.call())
     class(res) <- "envelope_test"
     res
 }
@@ -205,8 +205,8 @@ as_quantile_envelope <- function(curve_set, alpha=0.05, ...) {
     UB <- EX + talpha*(QQ[2,]-EX);
 
     res <- list(r=curve_set[['r']], method="Asymmetric quantile envelope test", p=p,
-            central_curve=EX, data_curve=data_curve, lower=LB, upper=UB,
-            call=match.call())
+                central_curve=EX, data_curve=data_curve, lower=LB, upper=UB,
+                call=match.call())
     class(res) <- "envelope_test"
     res
 }
@@ -241,9 +241,9 @@ normal_envelope <- function(curve_set, alpha=0.05, n_norm=200000, ...) {
     simnorm <- rmvnorm(n=n_norm, mean = EX, sigma = varX, method=c('svd'));
 
     sdX <- as.vector(apply(stats, MARGIN=2, FUN=sd))
-#    sdX <- array(0,n);
-#    for(i in 1:n){sdX[i] <- sqrt(varX[i,i])}
-#    for(i in 1:n){if(sdX[i]==0){sdX[i]<-0.00000001}}
+    #    sdX <- array(0,n);
+    #    for(i in 1:n){sdX[i] <- sqrt(varX[i,i])}
+    #    for(i in 1:n){if(sdX[i]==0){sdX[i]<-0.00000001}}
     distance <- array(0, n_norm);
     for(j in 1:n_norm) {
         ttt <- abs(simnorm[j,]-EX)/sdX
@@ -259,11 +259,11 @@ normal_envelope <- function(curve_set, alpha=0.05, n_norm=200000, ...) {
 
     #-- calculate the p-value
     p <- spptest:::estimate_p_value(obs=tmaxd, sim_vec=distance, ...)
-#    p <- 1;
-#    for(i in 1:n_norm) {
-#        if(distance[i]>tmaxd) p<-p+1;
-#    }
-#    p <- p/(n_norm+1);
+    #    p <- 1;
+    #    for(i in 1:n_norm) {
+    #        if(distance[i]>tmaxd) p<-p+1;
+    #    }
+    #    p <- p/(n_norm+1);
 
     #-- calculate the 100(1-alpha)% envelopes
     talpha <- distancesorted[round((1-alpha)*n_norm)];
@@ -271,8 +271,8 @@ normal_envelope <- function(curve_set, alpha=0.05, n_norm=200000, ...) {
     UB <- EX + talpha*sdX
 
     res <- list(r=curve_set[['r']], method="Approximative normal envelope test", p=p,
-            central_curve=EX, data_curve=data_curve, lower=LB, upper=UB,
-            call=match.call())
+                central_curve=EX, data_curve=data_curve, lower=LB, upper=UB,
+                call=match.call())
     class(res) <- "envelope_test"
     res
 }
