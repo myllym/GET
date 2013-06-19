f1a0ddfca299911d2e170e6bbb629a2b54f08b7d
Mari Myllymäki <mari.myllymaki@aalto.fi>
2013-06-19 21:13:39 +0300

    Add random_labeling.

--- R/wrapper.r
+++ R/wrapper.r
@@ -1,3 +1,38 @@
+#' Random labeling with K_f functions
+#'
+#' Perform simulations under the random labelling for a marked point pattern
+#' and estimate K_f- or L_f-functions for the pattern and simulations.
+#'
+#' @inheritParams marksummary::summ_func_random_labelling
+#' @inheritParams crop_curves
+#' @inheritParams residual
+#' @seealso \code{\link{rank_envelope}}, \code{\link{st_envelope}}, \code{\link{qdir_envelope}}, \code{\link{deviation}}
+#' @references
+#' Myllymäki, M., Grabarnik, P., Seijo, H. and Stoyan. D. (2013). Deviation test construction and power comparison for marked spatial point patterns. arXiv:1306.1028
+#' @export
+random_labelling <- function(pattern,
+        mtf_name = 'm', nsim = 999L,
+        r_min = NULL, r_max = NULL, r_vec = NULL,
+        use_L = TRUE,
+        edge_correction = 'translate',
+        use_theo = TRUE, method = 'permute',
+        use_biased_lambda2 = FALSE) {
+    if (use_L) {
+        func <- random_labelling_L_f
+    } else {
+        func <- random_labelling_K_f
+    }
+
+    curve_set <- func(pattern, mtf_name = mtf_name, r_max = r_max,
+            r_vec = r_vec, n_sim = nsim, calc_theo = use_theo,
+            edge_correction = edge_correction, method = method,
+            use_biased_lambda2 = use_biased_lambda2)
+    # r_max has already been dealt with in marksummary.
+    curve_set <- crop_curves(curve_set, r_min = r_min, r_max = NULL)
+    curve_set <- residual(curve_set, use_theo = use_theo)
+    curve_set
+}
+
 # FIXME: inheritParams does not work in the expected way here.
 #' Do a random labelling deviation test for a pattern using K_f- or
 #' L_f-functions.
