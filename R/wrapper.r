814c2f83ac7a201a6dbad7e0965bcfed9d007d0f
Mari Myllymäki <mari.myllymaki@aalto.fi>
2013-06-19 20:30:25 +0300

    Add documentation for deviation_test

--- R/wrapper.r
+++ R/wrapper.r
@@ -37,10 +37,49 @@ random_labelling_test <- function(pattern,
     p
 }
 
-#' Scale the curve_set and perform a deviation test.
+#' Deviation test
+#'
+#' Crop the curve set to the interval of distances [r_min, r_max],
+#' calculate residuals, scale the residuals and perform a deviation test
+#' with a chosen deviation measure.
+#'
+#'
+#' The deviation test is based on a test function T(r) and it works as follows:
+#'
+#' 1) The test function estimated for the data, T_1(r), and for nsim simulations
+#' from the null model, T_2(r), ...., T_{nsim+1}(r), must be saved in 'curve_set'
+#' and given to the deviation_test function.
+#'
+#' 2) The deviation_test function then
+#'\itemize{
+#'   \item Crops the functions to the chosen range of distances [r_min, r_max].
+#'   \item Calculates the residuals d_i(r) = T_i(r) - T_0(r), where T_0(r) is
+#'     the expectation of T(r) under the null hypothesis.
+#'     If use_theo = TRUE, as T_0(r) the theoretical value given in the curve_set$theo
+#'     is used, if it is given. Otherwise, T_0(r) is estimated by the mean of 
+#'     T_j(r), j=2,...,nsim+1.
+#'   \item Scales the residuals. Options are
+#'         \itemize{
+#'           \item 'none' No scaling. Nothing done.
+#'           \item 'q' Quantile scaling.
+#'           \item 'qdir' Directional quantile scaling.
+#'           \item 'st' Studentised scaling.
+#'         }
+#'   \item Calculates the global deviation measure u_i, i=1,...,nsim+1, see options for 'measure'.
+#'   \item Calculates the p-value.
+#'}
 #'
 #' @inheritParams convert_envelope
 #' @inheritParams crop_curves
+#' @inheritParams residual
+#' @inheritParams deviation
+#' @param scaling The name of the scaling to use. Options include 'none',
+#'   'q', 'qdir' and 'st'. 'qdir' is default.
+#' @return If 'savedevs=FALSE' (default), the p-value is returned.
+#' If 'savedevs=TRUE', then a list containing the p-value and calculated deviation measures
+#' u_i, i=1,...,nsim+1 (where u_1 corresponds to the data pattern) is returned.
+#' @references
+#' Myllymäki, M., Grabarnik, P., Seijo, H. and Stoyan. D. (2013). Deviation test construction and power comparison for marked spatial point patterns. arXiv:1306.1028
 #' @export
 #' @examples
 #' ## Testing complete spatial randomness (CSR)
