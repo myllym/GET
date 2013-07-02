06140f1d801f68ab159045a4658b776ad4f6d8da
Mari Myllymaki <mari.j.myllymaki@gmail.com>
2013-07-02 16:50:33 +0300

    Add a comment.

--- R/envelopes.r
+++ R/envelopes.r
@@ -447,6 +447,7 @@ qdir_envelope <- function(curve_set, alpha=0.05, savedevs=FALSE, probs = c(0.025
     Nsim <- dim(sim_curves)[1];
     nr <- dim(sim_curves)[2]
 
+    # Define T_0 and residual curve_set
     if(!curve_set$is_residual) {
         if(with(curve_set, exists('theo'))) {
             T_0 <- curve_set[['theo']]
