385bb527d62859c542b950721ffc05c06fe73098
Mari Myllymaki <mari.j.myllymaki@gmail.com>
2015-06-26 14:58:08 +0300

    Update references.

--- R/spptest-package.r
+++ R/spptest-package.r
@@ -148,10 +148,11 @@
 #' Pavel Grabarnik (gpya@@rambler.ru)
 #'
 #' @references
-#' Myllymäki, M., Grabarnik, P., Seijo, H. and Stoyan. D. (2013). Deviation test construction and power comparison for marked spatial point patterns. arXiv:1306.1028
+#' Myllymäki, M., Grabarnik, P., Seijo, H. and Stoyan. D. (2015). Deviation test construction and power comparison for marked spatial point patterns. Spatial Statistics 11, 19-34.
 #'
-#' Myllymäki, M., Mrkvička, T., Seijo, H. and Grabarnik, P. (2013). Global envelope tests for spatial point patterns. arXiv:1307.0239 [stat.ME]
+#' Myllymäki, M., Mrkvička, T., Grabarnik, P., Seijo, H. and Hahn, U. (2015). Global envelope tests for spatial point patterns. arXiv:1307.0239v3 [stat.ME]
 #'
+#' Mrkvička, T., Myllymäki, M. and Hahn, U. (2015). Multiple Monte Carlo testing with applications in spatial point processes. arXiv:1506.01646 [stat.ME]
 #' @name spptest
 #' @docType package
 #' @aliases spptest-package spptest
