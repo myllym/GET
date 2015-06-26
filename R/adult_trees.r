ef4016cf285ce4b0e51814e1a358ac340f1bbe5f
Mari Myllymaki <mari.j.myllymaki@gmail.com>
2015-06-26 17:48:45 +0300

    Add adult_trees and saplings data.

---
+++ R/adult_trees.r
@@ -0,0 +1,23 @@
+#' Adult trees data set
+#'
+#' A pattern of large trees (height > 25 m) originating from an uneven aged multi-species
+#' broadleaf nonmanaged forest in Kaluzhskie Zaseki, Russia.
+#'
+#' @format   An object of class \code{"ppp"}
+#' representing the point pattern of tree locations.
+#'
+#' See \code{\link[spatstat]{ppp.object}} for details of the format.
+#'
+#' @usage data(adult_trees)
+#' @references
+#' Grabarnik, P. and Chiu, S. N. (2002) Goodness-of-fit test for complete spatial randomness against
+#' mixtures of regular and clustered spatial point processes. Biometrika, 89, 411–421.
+#'
+#' van Lieshout, M.-C. (2010) Spatial point process theory. In Handbook of Spatial Statistics (eds. A. E.
+#' Gelfand, P. J. Diggle, M. Fuentes and P. Guttorp), Handbooks of Modern Statistical Methods. Boca
+#' Raton: CRC Press.
+#'
+#' @keywords datasets
+#' @keywords spatial
+#' @docType data
+#'
---
+++ R/saplings.r
@@ -0,0 +1,23 @@
+#' Saplings data set
+#'
+#' A pattern of small trees (height <= 15 m) originating from an uneven aged multi-species
+#' broadleaf nonmanaged forest in Kaluzhskie Zaseki, Russia.
+#'
+#' @format   An object of class \code{"ppp"}
+#' representing the point pattern of tree locations.
+#'
+#' See \code{\link[spatstat]{ppp.object}} for details of the format.
+#'
+#' @usage data(saplings)
+#' @references
+#' Grabarnik, P. and Chiu, S. N. (2002) Goodness-of-fit test for complete spatial randomness against
+#' mixtures of regular and clustered spatial point processes. \emph{Biometrika}, \bold{89}, 411–421.
+#'
+#' van Lieshout, M.-C. (2010) Spatial point process theory. In Handbook of Spatial Statistics (eds. A. E.
+#' Gelfand, P. J. Diggle, M. Fuentes and P. Guttorp), Handbooks of Modern Statistical Methods. Boca
+#' Raton: CRC Press.
+#'
+#' @keywords datasets
+#' @keywords spatial
+#' @docType data
+#'
