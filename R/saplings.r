add279e084d7b511860082e48173d1c3e11fc6bf
Myllymäki Mari <mari.myllymaki@luke.figit config --global --edit>
2017-09-28 10:51:02 +0300

    Change example nsim.

--- R/saplings.r
+++ R/saplings.r
@@ -71,7 +71,8 @@
 #' fitted_model <- kppm(saplings~1, clusters = "MatClust", statistic="pcf")
 #' summary(fitted_model)
 #'
-#' nsim <- 499 # 19 for experimenting; 499 ok for test = 'qdir'
+#' nsim <- 19 # 19 just for experimenting
+#' #nsim <- 499 # ok for test = 'qdir' (takes about 1,6 h)
 #'
 #' # Make the adjusted directional quantile global envelope test using the L(r)-r function
 #' # (For the rank envelope test, choose test = "rank" instead and increase nsim.)
