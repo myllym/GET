6dd39d5c15436d52b52e78fe686bdf413ab8fcc4
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2018-03-09 16:21:06 +0200

    Hardcode type = 1 for quantile calculation (curve_set_quant).

--- R/curve-set.r
+++ R/curve-set.r
@@ -428,10 +428,11 @@ curve_set_sd <- function(curve_set) {
 #
 # If obs is a matrix, then take the quantiles of the functions in obs. (ignore sim_m)
 # If obs is a vector, then take the quantiles of the functions in sim_m.
+# @param ... Ignored. type = 1 set for the quantile calculation.
 #' @importFrom stats quantile
 curve_set_quant <- function(curve_set, probs, ...) {
-  if(with(curve_set, is.matrix(obs))) quant_m <- apply(curve_set[['obs']], 1, stats::quantile, probs = probs, ...)
-  else quant_m <- apply(curve_set[['sim_m']], 1, stats::quantile, probs = probs, ...)
+  if(with(curve_set, is.matrix(obs))) quant_m <- apply(curve_set[['obs']], 1, stats::quantile, probs = probs, type = 1)
+  else quant_m <- apply(curve_set[['sim_m']], 1, stats::quantile, probs = probs, type = 1)
   # Dimensions: 2, r_idx.
   quant_m
 }
