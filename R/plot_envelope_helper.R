0047d059b26cd8e9cc1a8af39dc2e83f778926f8
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2018-07-05 01:14:38 +0300

    Increase indent.

--- R/plot_envelope_helper.R
+++ R/plot_envelope_helper.R
@@ -351,10 +351,11 @@ env_ggplot <- function(x, base_size, main, ylim, xlab, ylab, separate_yaxes=FALS
                     )
         if(rdata$retick_xaxis) {
             p <- p + ggplot2::scale_x_continuous(name = xlab,
-                    breaks = rdata$loc_break_values,
-                    labels = paste(round(rdata$r_break_values, digits=2),
-                            limits = range(rdata$new_r_values)))
-            p <- p + ggplot2::geom_vline(xintercept = rdata$new_r_values[rdata$r_values_newstart_id], linetype = "dotted")
+                                                 breaks = rdata$loc_break_values,
+                                                 labels = paste(round(rdata$r_break_values, digits=2),
+                                                 limits = range(rdata$new_r_values)))
+            p <- p + ggplot2::geom_vline(xintercept = rdata$new_r_values[rdata$r_values_newstart_id],
+                                         linetype = "dotted")
         }
         else p <- p + ggplot2::scale_x_continuous(name = xlab)
         print(p)
