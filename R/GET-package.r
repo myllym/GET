eff7dd520f2a8942cdedf2c0073ffcd949cdd996
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2018-11-20 13:17:38 +0200

    Delete a link to dg.combined_global_envelope.

--- R/GET-package.r
+++ R/GET-package.r
@@ -81,7 +81,6 @@
 #' \item \emph{Adjusted} global envelope tests for composite hypotheses
 #' \itemize{
 #'   \item \code{\link{dg.global_envelope}}, see a detailed example in \code{\link{saplings}}
-#'   \item \code{\link{dg.combined_global_envelope}} for combined adjusted tests
 #' }
 #' \item \emph{One-way functional ANOVA}:
 #'  \itemize{
