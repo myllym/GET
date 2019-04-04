429f532adb3f282a30a304c0d035e94868352321
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-04-04 15:30:06 +0300

    Add comment.

--- R/envelopes.r
+++ R/envelopes.r
@@ -211,7 +211,7 @@ individual_global_envelope_test <- function(curve_set, type = "erl", alpha = 0.0
   res
 }
 
-# Functionality for combined_central_region and combined_global_envelope_test
+# Functionality for combined_central_region and combined_global_envelope_test (two-step procedure)
 combined_CR_or_GET <- function(curve_sets, CR_or_GET = c("CR", "GET"), coverage, ...) {
   ntests <- length(curve_sets)
   if(ntests < 1) stop("Only one curve_set, no combining to be done.\n")
