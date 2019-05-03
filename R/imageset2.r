e24511177dd018d24995888887cce218dff8a316
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-05-03 18:07:23 +0300

    Update help for imageset1 and 2.

--- R/imageset2.r
+++ R/imageset2.r
@@ -5,17 +5,19 @@
 #'
 #' We considered a categorical factor \code{Group} obtaining the values 1 or 2
 #' according to the group to which the image belongs to (10 images in the first group,
-#' 10 images in the second). The images were simulated in the square window [-1,1]^2 from the
+#' 10 images in the second), and a continous factor z that was generated from the uniform
+#' distribution on (0,1). The images were simulated in the square window [-1,1]^2 from the
 #' general linear model (GLM)
-#' \deqn{Y(r) = \exp(-10\cdot ||r||) \cdot g + \epsilon(r),}{Y(r) = exp(-10 ||r||) g + e(r),}
+#' \deqn{Y(r) = \exp(-10\cdot ||r||) \cdot (g + z) + \epsilon(r),}{Y(r) = exp(-10 ||r||) (g + z) + e(r),}
 #' where ||r|| denotes the Euclidean distance of the pixel to the origin, g is the group and
 #' the error stems from an inhomogeneous distribution over $I$ with the normal and
 #' bimodal errors in the middle and periphery of the image:
 #' \deqn{\epsilon(r) = \mathbb I (\|r\| \leq 0.5) G(r) + \mathbb I (\|r\| > 0.5) \frac{1}{2}G(r)^{1/5},}{e(r) = 1 (||r|| <= 0.5) G(r) + 1(||r|| > 0.5) 0.5 G(r)^{1/5},}
 #' where G(r) is a Gaussian random field with the exponential correlation structure
 #' with scale parameter 0.15 and standard deviation 0.2.
-#' @format A list of the \code{image_set} containing the simulated images, and
-#' the discrete group factor in the list component \code{Group}.
+#' @format A list of the \code{image_set} containing the simulated images,
+#' the discrete group factor in the list component \code{Group},
+#' and the continuous factor z in the list component \code{z}.
 #'
 #' @usage data(imageset1)
 #' @references
