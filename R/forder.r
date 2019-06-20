f022eadb5e90c84ac1e32c82c3958db15c22067e
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-06-20 18:44:47 +0300

    Update implementation of the area rank measure.
    Simplify the code due to simplified formula and add scaling so that the area measure lies between 0 and 1.

--- R/forder.r
+++ R/forder.r
@@ -103,7 +103,11 @@ individual_forder <- function(curve_set, r_min = NULL, r_max = NULL,
              RRRm <- apply(allranks, MARGIN=1, FUN=min)
              RRRm <- ceiling(RRRm) # = R_i
              distance <- array(0, Nfunc)
-             for(j in 1:Nfunc) distance[j] <- -(sum(RRRm[j] - allranks[j, allranks[j,] <= RRRm[j]]) + (max(RRRm)-RRRm[j])*nr)
+             for(j in 1:Nfunc) distance[j] <- sum(pmin(RRRm[j], allranks[j]))
+             if(alternative == "two.sided")
+               distance <- distance / (ceiling(Nfunc/2)*nr)
+             else
+               distance <- distance / ((Nfunc-1)*nr)
            })
   }
   names(distance) <- rownames(data_and_sim_curves)
