0f68e3b1d9ef17bc0670bbcd86faaf80119cb029
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-07-01 20:51:32 +0300

    Change latex \mathbb{1}->\mathbf{1}.

--- R/imageset1.r
+++ R/imageset1.r
@@ -7,11 +7,11 @@
 #' according to the group to which the image belongs to (10 images in the first group,
 #' 10 images in the second). The images were simulated in the square window [-1,1]^2 from the
 #' general linear model (GLM)
-#' \deqn{Y(r) = \exp(-10\cdot ||r||) \cdot (1+g) + \epsilon(r),}{Y(r) = exp(-10 ||r||) (1+g) + e(r),}
+#' \deqn{Y(r) = \exp(-10\cdot ||r||) \cdot (1 + g) + \epsilon(r),}{Y(r) = exp(-10 ||r||) (1 + g) + e(r),}
 #' where ||r|| denotes the Euclidean distance of the pixel to the origin, g is the group and
 #' the error stems from an inhomogeneous distribution over $I$ with the normal and
 #' bimodal errors in the middle and periphery of the image:
-#' \deqn{\epsilon(r) = \mathbb I (\|r\| \leq 0.5) G(r) + \mathbb I (\|r\| > 0.5) \frac{1}{2}G(r)^{1/5},}{e(r) = 1 (||r|| <= 0.5) G(r) + 1(||r|| > 0.5) 0.5 G(r)^{1/5},}
+#' \deqn{\epsilon(r) = \mathbf{1}(\|r\| \leq 0.5) G(r) + \mathbf{1}(\|r\| > 0.5) \frac{1}{2}G(r)^{1/5},}{e(r) = 1(||r|| <= 0.5) G(r) + 1(||r|| > 0.5) 0.5 G(r)^{1/5},}
 #' where G(r) is a Gaussian random field with the exponential correlation structure
 #' with scale parameter 0.15 and standard deviation 0.2.
 #' @format A list of the \code{image_set} containing the simulated images, and
