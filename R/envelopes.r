84b907fa0929e49e00a96fded9e3ff129c94a88a
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-07-03 23:06:28 +0300

    Change place of on.exit() right after the setting of par.

--- R/envelopes.r
+++ R/envelopes.r
@@ -535,11 +535,11 @@ plot.combined_global_envelope <- function(x, plot_style = c("ggplot2", "fv", "ba
              ncols_of_plots <- min(n_of_plots, max_ncols_of_plots)
              nrows_of_plots <- ceiling(n_of_plots / ncols_of_plots)
              opar <- par(mfrow=c(nrows_of_plots, ncols_of_plots))
+             on.exit(par(opar))
              if(is.vector(ylim) & length(ylim)==2) ylim <- rep(list(ylim), times=n_of_plots)
              for(i in 1:length(x))
                spatstat::plot.fv(x[[i]],
                                  main=labels[i], ylim=ylim[[i]], xlab=xlab[[i]], ylab=ylab[[i]], add=FALSE, ...)
-             on.exit(par(opar))
            },
            ggplot2 = {
              env_ggplot(x, base_size=base_size,
