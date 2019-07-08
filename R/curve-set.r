cfefae1e60e2ce21263244e2361628cfa9e47416
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-07-08 11:59:32 +0300

    Space change.

--- R/curve-set.r
+++ R/curve-set.r
@@ -286,7 +286,7 @@ is.curve_set <- function(x) inherits(x, 'curve_set')
 #' @export
 #' @importFrom utils str
 print.curve_set <- function(x, ...) {
-    utils::str(x)
+  str(x)
 }
 
 #' Plot method for the class 'curve_set'
