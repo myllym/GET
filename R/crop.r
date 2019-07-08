4af885522cdda80326793439cb066d42677f64e8
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-07-08 13:33:52 +0300

    Space change/crop.

--- R/crop.r
+++ R/crop.r
@@ -13,58 +13,60 @@
 #'   the cropped radius vector.
 #' @export
 crop_curves <- function(curve_set, r_min = NULL, r_max = NULL) {
-    curve_set <- convert_envelope(curve_set, allow_Inf_values = TRUE)
+  curve_set <- convert_envelope(curve_set, allow_Inf_values = TRUE)
 
-    n_r_min <- length(r_min)
-    if (n_r_min > 0L && (n_r_min != 1L || !is.finite(r_min))) {
-        stop('r_min must be a finite scalar value or NULL.')
+  n_r_min <- length(r_min)
+  if(n_r_min > 0L && (n_r_min != 1L || !is.finite(r_min))) {
+    stop('r_min must be a finite scalar value or NULL.')
+  }
+
+  n_r_max <- length(r_max)
+  if(n_r_max > 0L && (n_r_max != 1L || !is.finite(r_max))) {
+    stop('r_max must be a finite scalar value or NULL.')
+  }
+
+  r <- curve_set[['r']]
+
+  if(n_r_min == 1L) {
+    if(n_r_max == 1L) {
+      if(r_min >= r_max) {
+        stop('r_min must be smaller than r_max.')
+      }
+      cut_idx <- which(r >= r_min & r <= r_max)
     }
-
-    n_r_max <- length(r_max)
-    if (n_r_max > 0L && (n_r_max != 1L || !is.finite(r_max))) {
-        stop('r_max must be a finite scalar value or NULL.')
+    else {
+      cut_idx <- which(r >= r_min)
     }
-
-    r <- curve_set[['r']]
-
-    if (n_r_min == 1L) {
-        if (n_r_max == 1L) {
-            if (r_min >= r_max) {
-                stop('r_min must be smaller than r_max.')
-            }
-            cut_idx <- which(r >= r_min & r <= r_max)
-        } else {
-            cut_idx <- which(r >= r_min)
-        }
-    } else {
-        if (n_r_max == 1L) {
-            cut_idx <- which(r <= r_max)
-        } else {
-            return(check_curve_set_content(curve_set, allow_Inf_values = FALSE))
-        }
+  } else {
+    if(n_r_max == 1L) {
+      cut_idx <- which(r <= r_max)
     }
-
-    if (length(cut_idx) < 1L) {
-        stop('r_min and r_max cropped everything away.')
+    else {
+      return(check_curve_set_content(curve_set, allow_Inf_values = FALSE))
     }
+  }
 
-    r_cut <- r[cut_idx]
-    if(is.vector(curve_set[['obs']])) obs_cut <- curve_set[['obs']][cut_idx]
-    else obs_cut <- curve_set[['obs']][cut_idx, , drop = FALSE]
-    sim_m_cut <- curve_set[['sim_m']][cut_idx, , drop = FALSE]
-    theo <- curve_set[['theo']]
-    n_theo <- length(theo)
-    if (n_theo > 0L) {
-        theo_cut <- theo[cut_idx]
-    }
+  if(length(cut_idx) < 1L) {
+    stop('r_min and r_max cropped everything away.')
+  }
 
-    res <- list(r=r_cut, obs=obs_cut)
-    if(!is.null(sim_m_cut)) res[['sim_m']] <- sim_m_cut
-    if (n_theo > 0L) {
-        res[['theo']] <- theo_cut
-    }
-    if(with(curve_set, exists('is_residual'))) res[['is_residual']] <- curve_set[['is_residual']]
+  r_cut <- r[cut_idx]
+  if(is.vector(curve_set[['obs']])) obs_cut <- curve_set[['obs']][cut_idx]
+  else obs_cut <- curve_set[['obs']][cut_idx, , drop = FALSE]
+  sim_m_cut <- curve_set[['sim_m']][cut_idx, , drop = FALSE]
+  theo <- curve_set[['theo']]
+  n_theo <- length(theo)
+  if(n_theo > 0L) {
+    theo_cut <- theo[cut_idx]
+  }
 
-    res <- create_curve_set(res, allow_Inf_values = FALSE)
-    res
+  res <- list(r=r_cut, obs=obs_cut)
+  if(!is.null(sim_m_cut)) res[['sim_m']] <- sim_m_cut
+  if(n_theo > 0L) {
+    res[['theo']] <- theo_cut
+  }
+  if(with(curve_set, exists('is_residual'))) res[['is_residual']] <- curve_set[['is_residual']]
+
+  res <- create_curve_set(res, allow_Inf_values = FALSE)
+  res
 }
