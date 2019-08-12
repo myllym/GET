8c42e2e47d0f4f780bb3670cdccd8912ee71ca5d
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2019-08-12 23:09:22 +0300

    Edit default mains of 2d plots.
    Remove "(red)" from "Sign. below (red)" and "Sign. above (red)" as the color is allowed to be specified by the user (red is the default color).

--- R/envelopes2d.r
+++ R/envelopes2d.r
@@ -306,8 +306,8 @@ plot.combined_global_envelope2d <- function(x, plot_style = c("ggplot2", "basic"
              main.plots <- paste(names(x)[i], ": ", c("Observed",
                                                       "Lower envelope",
                                                       "Upper envelope",
-                                                      "Sign. below (red)",
-                                                      "Sign. above (red)"), sep="")
+                                                      "Sign. below",
+                                                      "Sign. above"), sep="")
              env2d_basic_plot(x[[i]], var='obs', sign.col=sign.col, transparency=transparency, contours=contours, main=main.plots[1], ...)
              env2d_basic_plot(x[[i]], var='lo', sign.col=sign.col, transparency=transparency, contours=contours, main=main.plots[2], ...)
              env2d_basic_plot(x[[i]], var='hi', sign.col=sign.col, transparency=transparency, contours=contours, main=main.plots[3], ...)
