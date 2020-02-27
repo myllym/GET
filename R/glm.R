309c59748e5c93bae90b1a23140eb292e4ab96ba
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2020-02-27 13:55:04 +0200

    Add a helper function.
    The function check_isnested checks nestedness of a formula within another. It is used by GET.spatialF and functions for GLM.

--- R/glm.R
+++ R/glm.R
@@ -1,3 +1,12 @@
+# Check that formula.reduced is nested within formula.full and includes something extra.
+check_isnested <- function(formula.full, formula.reduced) {
+  # Check that the reduced model is nested within the full model
+  if(!all(labels(terms(formula.reduced)) %in% labels(terms(formula.full)))) stop("The reduced model includes some extra variables, not in the full model.\n")
+  if(attr(terms(formula.full), "intercept") < attr(terms(formula.reduced), "intercept")) stop("The reduced model includes intercept, but the full model does not.\n")
+  # Check that the full model includes something in addition to the reduced model
+  if(all(labels(terms(formula.full)) %in% labels(terms(formula.reduced)))) stop("The full model should not be equal to the reduced model.\n")
+}
+
 # Preliminary checks for the graph.flm and frank.flm
 #' @importFrom stats terms
 flm.checks <- function(nsim, formula.full, formula.reduced, curve_sets, factors = NULL, fast = TRUE) {
