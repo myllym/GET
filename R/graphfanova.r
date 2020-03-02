991d589d82f3870085cf545a688570233638687b
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2020-03-03 01:18:24 +0200

    Refer correctly to the changed data set cgec.

--- R/graphfanova.r
+++ R/graphfanova.r
@@ -278,16 +278,16 @@ contrasts.m <- function(x, groups, ...) {
 #' nsim <- 2499 # increase to reduce Monte Carlo error
 #'
 #' # Test for unequal lag 1 covariances
-#' res.cov1 <- graph.fanova(nsim = nsim, curve_set = cgec,
-#'                          groups = attr(cgec, "group"),
+#' res.cov1 <- graph.fanova(nsim = nsim, curve_set = cgec$cgec,
+#'                          groups = cgec$group,
 #'                          test.equality = "cov", cov.lag = 1)
 #' plot(res.cov1, ncol=3,
 #'      labels = paste("Group ", 1:3, sep=""),
 #'      xlab=substitute(paste(i, " (", italic(j), ")", sep=""), list(i="Year", j="r")),
 #'      ylab=expression(italic(bar(W)[i](r))))
 #' # Test for equality of variances among groups
-#' res.var <- graph.fanova(nsim = nsim, curve_set = cgec,
-#'                         groups = attr(cgec, "group"),
+#' res.var <- graph.fanova(nsim = nsim, curve_set = cgec$cgec,
+#'                         groups = cgec$group,
 #'                         test.equality = "var")
 #' plot(res.var, ncol=3,
 #'      labels = paste("Group ", 1:3, sep=""),
@@ -296,8 +296,8 @@ contrasts.m <- function(x, groups, ...) {
 #'
 #' # Test for equality of means assuming equality of variances
 #' # a) using 'means'
-#' res <- graph.fanova(nsim = nsim, curve_set = cgec,
-#'                     groups = attr(cgec, "group"),
+#' res <- graph.fanova(nsim = nsim, curve_set = cgec$cgec,
+#'                     groups = cgec$group,
 #'                     variances = "equal",
 #'                     contrasts = FALSE)
 #' plot(res, ncol=3,
@@ -305,14 +305,14 @@ contrasts.m <- function(x, groups, ...) {
 #'      xlab=substitute(paste(i, " (", italic(j), ")", sep=""), list(i="Year", j="r")),
 #'      ylab=expression(italic(bar(T)[i](r))))
 #' # b) using 'contrasts'
-#' res2 <- graph.fanova(nsim = nsim, curve_set = cgec,
-#'                     groups = attr(cgec, "group"),
+#' res2 <- graph.fanova(nsim = nsim, curve_set = cgec$cgec,
+#'                     groups = cgec$group,
 #'                     variances = "equal",
 #'                     contrasts = TRUE)
 #' }
 #' \dontshow{
-#' res2 <- graph.fanova(nsim = 4, curve_set = cgec,
-#'                     groups = attr(cgec, "group"),
+#' res2 <- graph.fanova(nsim = 4, curve_set = cgec$cgec,
+#'                     groups = cgec$group,
 #'                     variances = "equal",
 #'                     contrasts = TRUE,
 #'                     alpha = 0.2)
