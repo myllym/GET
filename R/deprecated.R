06cac020e10b69e05c7fd27a9cd01acf4f250ca1
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2020-03-27 18:33:12 +0200

    Fix param of deprecated functions.
    To still work as earlier, appropriate new image_set objects which are now curve_set objects.

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,25 +1,32 @@
 #' @export
-global_envelope_test2d <- function(..., image_sets=image_sets) {
+central_region2d <- function(image_sets, ...) {
+  warning("central_region2d is deprecated. Please use central_region.")
+  central_region(curve_sets=image_sets, ...)
+}
+#' @export
+global_envelope_test2d <- function(image_sets, ...) {
   warning("global_envelope_test2d is deprecated. Please use global_envelope_test.")
-  global_envelope_test(..., curve_sets=image_sets)
+  global_envelope_test(curve_sets=image_sets, ...)
 }
 #' @export
-graph.fanova2d <- function(..., image_set=image_set) {
+graph.fanova2d <- function(nsim, image_set, groups, ...) {
   warning("graph.fanova2d is deprecated. Please use graph.fanova.")
-  graph.fanova(..., curve_set=image_set)
+  graph.fanova(nsim=nsim, curve_set=image_set, groups=groups, ..., n.aver=1)
 }
 #' @export
-graph.flm2d <- function(..., image_sets=image_sets) {
-  warning("graph.flm2d is deprecated. Please use graph.flm.")
-  graph.flm(..., curve_sets=image_sets)
-}
-#' @export
-frank.fanova2d <- function(..., image_set=image_set) {
+frank.fanova2d <- function(nsim, image_set, groups, ...) {
   warning("frank.fanova2d is deprecated. Please use frank.fanova.")
-  frank.fanova(..., curve_set=image_set)
+  frank.fanova(nsim=nsim, curve_set=image_set, groups=groups, ...)
 }
 #' @export
-frank.flm2d <- function(..., image_sets=image_sets) {
+graph.flm2d <- function(nsim, formula.full, formula.reduced, image_sets, factors = NULL, ...) {
+  warning("graph.flm2d is deprecated. Please use graph.flm.")
+  graph.flm(nsim=nsim, formula.full=formula.full, formula.reduced=formula.reduced,
+            curve_sets=image_sets, factors=factors, ...)
+}
+#' @export
+frank.flm2d <- function(nsim, formula.full, formula.reduced, image_sets, factors = NULL, ...) {
   warning("frank.flm2d is deprecated. Please use frank.flm.")
-  frank.flm(..., curve_sets=image_sets)
+  frank.flm(nsim=nsim, formula.full=formula.full, formula.reduced=formula.reduced,
+            curve_sets=image_sets, factors=factors, ...)
 }
