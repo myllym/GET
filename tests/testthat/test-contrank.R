1c82b6ec358eb579126169bb9263b99c0ec1f3d2
Mikko Kuronen <mikko.p.kuronen@gmail.com>
2022-10-07 08:34:31 +0300

    More tests for contrank.

--- tests/testthat/test-contrank.R
+++ tests/testthat/test-contrank.R
@@ -1,12 +1,13 @@
 test_that("contrank", {
+  expectrank <- function(x) {
+    r <- rank(x, ties.method = "average")
+    cr <- contrank(x)
+    expect_equal(all(r-1 <= cr & cr <= r), TRUE)
+  }
+  expectrank(c(1,2,2))
+  expectrank(c(2,2,3))
   for(n in c(1,2,3,4,5,6,10, 100)) {
-    x <- rnorm(n)
-    r <- rank(x, ties.method = "average")
-    cr <- contrank(x)
-    expect_equal(all(r-1 <= cr & cr <= r), TRUE)
-    x <- sample(1:3, n, replace=TRUE)
-    r <- rank(x, ties.method = "average")
-    cr <- contrank(x)
-    expect_equal(all(r-1 <= cr & cr <= r), TRUE)
+    expectrank(rnorm(n))
+    expectrank(sample(1:3, n, replace=TRUE))
   }
 })
