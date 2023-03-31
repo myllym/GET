2d0a0be079e1754da4eeea6bfb3b7a30d5530f58
Myllymäki Mari <mari.j.myllymaki@gmail.com>
2023-03-31 16:42:11 +0300

    Print all multiple envelope levels in default title.
    Concerns the title of plot of central regions where multiple covarages are
    presented simultaneously.

--- R/plot_envelope_helper.R
+++ R/plot_envelope_helper.R
@@ -74,8 +74,12 @@ env_main_default <- function(x, digits=3, alternative=get_alternative(x)) {
         if(!is.null(attr(x, "alpha"))) {
           if(inherits(x, c("fboxplot", "combined_fboxplot")))
             main <- paste0(attr(x, "method"), " based on ", 100*(1-attr(x, "alpha")), "% central region (", attr(x, "type"), ")")
-          else if(inherits(x, c("global_envelope", "combined_global_envelope")))
-            main <- paste0(100*(1-attr(x, "alpha")), "% central region (", attr(x, "type"), ")")
+          else if(inherits(x, c("global_envelope", "combined_global_envelope"))) {
+            main <- NULL
+            for(i in seq_along(attr(x, "alpha")))
+              main <- paste0(main, 100*(1-attr(x, "alpha")[i]), "% ")
+            main <- paste0(main, "central region (", attr(x, "type"), ")")
+          }
           else
             main <- NULL
         }
